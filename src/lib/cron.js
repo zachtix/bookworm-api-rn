803f82362e280331cd6613913459a9687733e7e3
Atthawut Smith <38793720+zachtix@users.noreply.github.com>
2026-02-25 22:11:08 +0700

    cronjob

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/1 * * * *", function () {
+const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
